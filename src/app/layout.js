005ec42a57f6d690af1883566346fdd209889dd5
julsCadenas <jscadenas12@gmail.com>
2025-02-13 01:20:24 +0800

    updated favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,14 +14,12 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Valentine's Command line",
   description: "Happy Valentine's day!",
-  icons: {
-    icon: "/favicon.ico",
-  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
